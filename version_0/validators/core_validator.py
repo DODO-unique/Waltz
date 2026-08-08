79b8beb7820cdf8e0cf652f19bde5444512eaa34
V. Prescott <victorprescott554@gmail.com>
2026-08-08 17:06:00 +0530

    feat: add core_validators [inherited from legacy]

---
+++ version_0/validators/core_validator.py
@@ -0,0 +1,192 @@
+# --------------------- Imports
+
+from collections.abc import Callable
+from datetime import datetime
+from typing import Any, Literal
+from uuid import UUID, uuid4
+
+from pydantic import AnyHttpUrl, BaseModel, Field, model_validator
+from validation_helper import Addresses, Mail, Password, ProviderName, Uid, UserName
+
+from ..core.enums import OperationIntentions
+
+# ---------------- Cadence Schemas
+
+class CadencePayload(BaseModel):
+    """
+    Internal payload schema for sending email requests to relevant service set by developer.
+
+    ```python
+    email: Mail
+    code: str
+    ```
+
+    Notes:
+    - `code` is **not hashed**.
+    - It is the decimal string representation of an integer.
+    """
+    email : Mail
+    code : str
+
+
+class CadenceTicket(CadencePayload):
+    '''
+    A wrapper around CadencePayload.   
+    Important for requesting tickets to cadence decorator
+
+        ```python
+        id: UUID = uuid4()
+        payload: CadencePayload
+        ```
+    '''
+    id: UUID = Field(default_factory=uuid4)
+    payload: CadencePayload
+
+# ------------------ Identity Service
+
+class IdentityServiceSchema(BaseModel):
+    pass
+
+class BaseRegisterPayload(IdentityServiceSchema):
+    id: Uid = Field(default_factory=uuid4) # NOTE: sub comes here as string, local handles UUID
+    name: str | None
+    uname: str | None
+    mail: Mail | None
+    gender: Literal["male", "female", "non_binary", "prefer_not_to_say", "self_describe"] | None
+    birthdate: datetime | None
+    addr: Addresses | None
+    picture: AnyHttpUrl | None
+
+class OAuth(BaseRegisterPayload):
+    profile: AnyHttpUrl | None
+    website: AnyHttpUrl | None
+    zoneinfo: str | None
+    locale: str | None
+    updated_at: datetime # compare for changes
+
+class WaltzAuth(BaseRegisterPayload):
+    password: Password
+
+class IdentityPayload(IdentityServiceSchema):
+    '''
+    Schema of IdentityPayload:
+
+        uname: optional(UserName)   
+        email: optional(Mail)
+
+    Use internally to fetch Uid. 
+    '''
+    uname: UserName | None = None
+    email: Mail | None = None
+
+    @model_validator(mode="after")
+    def check_contact(self):
+        if self.email is None and self.uname is None:
+            raise ValueError("Either email or uname must be provided.")
+        return self
+
+class OAuthAuthenticateResponse(IdentityServiceSchema):
+    id: str
+    updated_at: datetime
+
+
+# ------------------ OAuth
+
+class OAuthSchema(BaseModel):
+    pass
+
+class TokenResponse(OAuthSchema):
+    access_token: str # They would usually always have one if they follow OAuth 2.0
+    token_type: str # almost always 'bearer'
+    expires_in: int | None = None
+    refresh_token: str | None = None
+    scope: str | None = None
+    id_token: str | None = None
+
+class TokenRequest(OAuthSchema):
+    grant_type: Literal["authorization_code"] = "authorization_code"
+    code: str
+    redirect_uri: AnyHttpUrl
+    client_id: str
+    client_secret: str | None = None
+    code_verifier: str | None = None
+
+
+class AuthorizationRequest(OAuthSchema):
+    response_type: Literal["code"] = "code" # you don't need to set it everytime, since default is code
+    client_id : str  # we get this from client
+    redirect_uri : AnyHttpUrl # from client, only one time though
+    scope: str # a space-delimited string 
+    state: str # random cryptographic stream
+    code_challenge: str | None = None # if you don't set it then it is None
+    code_challenge_method: Literal["S256"] | None = None
+
+class BaseAuthorizationResponse(OAuthSchema):
+    provider: "ProviderName"
+    state: str
+
+class AuthorizationSuccess(BaseAuthorizationResponse):
+    status: Literal["success"]
+    code: str
+
+class AuthorizationFailure(BaseAuthorizationResponse):
+    status: Literal["failure"]
+    error: str
+    error_description: str
+    error_url: str
+
+AuthorizationResponse = AuthorizationSuccess | AuthorizationFailure 
+
+# ------------------- Session
+
+class SessionRequest(BaseModel):
+    Uid: Uid | None = None
+    token: UUID = Field(default_factory=uuid4)
+
+
+# ------------------- SDK Schemas
+
+class SDKSchemas(BaseModel):
+    pass
+
+class OAuthCredentials(SDKSchemas):
+    client_id : str
+    client_secret : str | None
+
+    def with_provider_defaults(self, provider: ProviderName, uri: AnyHttpUrl) -> "Credentials":
+        return Credentials(
+            redirect_uri= uri,
+            provider=provider,
+            **self.model_dump()
+        )
+
+
+class DatabaseRegistry(BaseModel):
+    '''
+    A subset of Registry in listeners.
+        operation: User | Session | OneTimePassword
+        operator: Callable[[Any], Any]
+    
+    '''
+    operation: OperationIntentions[Any, Any]
+    operator: Callable[[Any], Any]
+
+class Credentials(OAuthCredentials):
+    redirect_uri : AnyHttpUrl
+    provider : ProviderName
+
+class Registry(SDKSchemas):
+    database : set[DatabaseRegistry]
+    cadence : Callable[[CadencePayload], None] | None
+    serenity : set[Credentials]
+
+class GrandRegistry(Registry):
+    registered_database_operation : set[OperationIntentions[Any, Any]]
+    registered_serenity_providers : set[ProviderName] 
+    # cadence, database, serenity
+
+class TicketType(BaseModel):
+    id: UUID = Field(default_factory=uuid4)
+    type: OperationIntentions[Any, Any]
+    payload: OAuth | WaltzAuth | IdentityPayload | Uid | SessionRequest
+
