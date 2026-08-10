b975d143b33399f14eb8598471e1a3b62f52c75d
V. Prescott <victorprescott554@gmail.com>
2026-08-10 17:18:00 +0530

    refactor: move payload from constructor to methods

--- version_0/core/idenity_service.py
+++ version_0/core/idenity_service.py
@@ -1,4 +1,4 @@
-from enums import User
+from enums import AuthResultEnum, Enum, User
 from general import compare_password, get_id, publish_ticket
 
 from ..validators.core_validator import (
@@ -8,7 +8,9 @@ from ..validators.core_validator import (
     TicketType,
     WaltzAuth,
 )
-from enums import AuthResultEnum, Enum
+from ..validators.endpoint_validators import (
+    LocalAuthPayload
+)
 
 
 class IdentityService:
@@ -17,42 +19,39 @@ class IdentityService:
     Register(payload)
     '''
 
-    def __init__(self, payload: WaltzAuth | OAuth | BaseRegisterPayload | None = None):
-        # NOTE: baseregisterpayload so if in Authentication OAuth payload does not contain any of the OAuth specifics
-        self.payload = payload
+    def __init__(self):
+        pass
         
-    async def register(self) -> None:
-        if self.payload is None:
-            raise ValueError("Payload is empty")
-        if isinstance(self.payload.id, str) and isinstance(self.payload, OAuth):
+    async def register(self, payload: OAuth | WaltzAuth) -> None:
+        if isinstance(payload, OAuth):
             await publish_ticket(
                 TicketType(
                 type=User.RegisterUserOAuth,
-                payload=self.payload
-                )
-            )
-
-        elif isinstance(self.payload, WaltzAuth):
-            await publish_ticket(
-                TicketType(
-                    type=User.RegisterUserLocal,
-                    payload=self.payload
+                payload=payload
                 )
             )
 
         else:
-            raise TypeError("Type or OAuth enum incorrect")
+            await publish_ticket(
+                TicketType(
+                    type=User.RegisterUserLocal,
+                    payload=payload
+                )
+            )
 
-    async def authenticate(self) -> Enum:
+        
+        raise TypeError("Type or OAuth enum incorrect")
+
+    async def authenticate(self, payload: OAuth | LocalAuthPayload) -> Enum:
 
         # check if payload is None (It can be None in certaincases so)
-        if self.payload is None:
+        if payload is None:
             raise ValueError("Payload is empty")
 
         # get a uid as we always fetch by uid
         uid = await get_id(IdentityPayload(
-            uname=self.payload.uname,
-            email=self.payload.mail,
+            uname=payload.uname,
+            email=payload.mail,
         ))
 
         # if uid is None then the user does not exist. So we return False as 'not authenticated'
@@ -60,7 +59,7 @@ class IdentityService:
             return AuthResultEnum.USER_NOT_FOUND
 
         # FOR OAuth the uid is always string
-        if isinstance(self.payload.id, str) and isinstance(uid, str):
+        if isinstance(uid, str):
             user: OAuth = await publish_ticket(
                 TicketType(
                     type=User.GetUserOAuth,
@@ -68,8 +67,8 @@ class IdentityService:
                 )
             )
 
-            if isinstance(self.payload, OAuth):
-                if user.updated_at == self.payload.updated_at:
+            if isinstance(payload, OAuth):
+                if user.updated_at == payload.updated_at:
                     return AuthResultEnum.SUCCESS
                 else:
                     # TODO: update_user is not set yet, raise a warning flag for it
@@ -80,7 +79,7 @@ class IdentityService:
         # if either uid and id aren't string then OAuth is not involved
         else:
             # checks if payload has password field
-            if not isinstance(self.payload, WaltzAuth):
+            if not isinstance(payload, WaltzAuth):
                 raise TypeError("INTERNAL: Payload is not WaltzAuth and is not OAuth (or uid is not str)")
             # get bassword in string
             password_body: str = await publish_ticket(
@@ -93,7 +92,7 @@ class IdentityService:
             # convert to bytes
             stored_pass = password_body.encode('utf-8')
             # check and get a predicate
-            is_authenticate = compare_password(self.payload.password.get_secret_value(), stored_pass)
+            is_authenticate = compare_password(payload.password.get_secret_value(), stored_pass)
 
             # return the predicate
             return AuthResultEnum.SUCCESS if is_authenticate else AuthResultEnum.NOT_AUTHENTIC
