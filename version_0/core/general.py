4517f961fbc3d545c64581934cc8f67f166daf2f
V. Prescott <victorprescott554@gmail.com>
2026-08-12 11:14:00 +0530

    refactor: move SHA hash function to security/

--- version_0/core/general.py
+++ version_0/core/general.py
@@ -1,4 +1,3 @@
-import bcrypt
 from enums import User
 
 from ..sdk.ticket_handler import TicketBus
@@ -10,19 +9,6 @@ async def publish_ticket(ticket: TicketType):
 
 async def dispatch_ticket(ticket: CadenceTicket):
     return await bus.dispatch(ticket)
-
-def hash_password(plain_text_password: str) -> bytes:
-    # first we convert string to bytes
-    password_bytes = plain_text_password.encode('utf-8')
-
-    # generate salt
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-    return hashed_password
-
-def compare_password(pt_pass: str, hpass: bytes) -> bool:
-    password_bytes = pt_pass.encode('utf-8')
-    return bcrypt.checkpw(password_bytes, hpass)
 
 async def get_id(identity: IdentityPayload) -> Uid | None:
     '''
